cdb63b26fc63fd87539d4b761e8b04547fbb68a1
Musaib shakeel <hajimusaib0@gmail.com>
2026-06-25 19:53:53 +0530

    Update Fibonacciseries.cpp

--- Fibonacciseries.cpp
+++ Fibonacciseries.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int a = 0, b = 1, c;
+
+    cout << "Enter number of terms: ";
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+        cout << a << " ";
+
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return 0;
+}
 
