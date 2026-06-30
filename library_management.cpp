34b36afcddf32632d09184ca1db086b23d615e43
Musaib shakeel <hajimusaib0@gmail.com>
2026-06-30 19:14:19 +0530

    Update library_management.cpp

--- library_management.cpp
+++ library_management.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Library
+{
+    int book_id;
+    string book_name;
+
+public:
+    void getData()
+    {
+        cout << "Enter Book ID: ";
+        cin >> book_id;
+
+        cout << "Enter Book Name: ";
+        cin >> book_name;
+    }
+
+    void display()
+    {
+        cout << "\nBook ID: " << book_id << endl;
+        cout << "Book Name: " << book_name << endl;
+    }
+};
+
+int main()
+{
+    Library b1;
+
+    b1.getData();
+    b1.display();
+
+    return 0;
+}
+
 
