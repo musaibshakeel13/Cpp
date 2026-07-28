c8160f8739b9a26679ac166b8f59b2aa2b1270c1
Musaib shakeel <hajimusaib0@gmail.com>
2026-07-28 20:37:51 +0530

    Update FriendFunctionProgram.cpp

--- FriendFunctionProgram.cpp
+++ FriendFunctionProgram.cpp
@@ -1,1 +1,31 @@
 
+
+#include <iostream>
+using namespace std;
+
+class A{
+    int x;
+
+public:
+    A(){
+        x = 10;
+    }
+
+    friend void show(A);
+};
+
+void show(A obj){
+    cout << obj.x;
+}
+
+int main(){
+    A o1;
+
+    show(o1);
+
+    return 0;
+}
+
+
+
+
