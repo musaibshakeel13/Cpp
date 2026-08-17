af78832cfb7bce97c1ef1f07cb441c8f243eeffd
Musaib shakeel <hajimusaib0@gmail.com>
2026-08-17 22:51:05 +0530

    Update rectangle_program.cpp

--- rectangle_program.cpp
+++ rectangle_program.cpp
@@ -1,1 +1,52 @@
 
+
+
+
+#include<iostream>
+using namespace std;
+
+class Rectangle {
+    int length, breadth;
+
+public:
+
+    // Parameterized Constructor
+    Rectangle(int l, int b) {
+        length = l;
+        breadth = b;
+    }
+
+    // Function to take input
+    void set() {
+        cin >> length >> breadth;
+    }
+
+    // Function to swap values
+    void mystery() {
+        int temp;
+        temp = length;
+        length = breadth;
+        breadth = temp;
+    }
+
+    // Function to calculate area
+    void area() {
+        cout << "Area = " << length * breadth << endl;
+    }
+};
+
+int main() {
+
+    // Object creation
+    Rectangle Obj1(100, 200);
+
+    Obj1.area();
+    Obj1.mystery();
+    Obj1.area();
+    Obj1.mystery();
+    Obj1.area();
+
+    return 0;
+}
+
+
