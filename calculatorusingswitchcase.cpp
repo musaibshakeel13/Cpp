f502bcd2ce6fc50add3c583f8d98d0859e1e9844
Musaib shakeel <hajimusaib0@gmail.com>
2026-08-27 21:57:28 +0530

    Update calculatorusingswitchcase.cpp

--- calculatorusingswitchcase.cpp
+++ calculatorusingswitchcase.cpp
@@ -1,1 +1,52 @@
 
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int choice;
+    float a, b;
+
+    cout << "----- Calculator -----" << endl;
+    cout << "1. Addition" << endl;
+    cout << "2. Subtraction" << endl;
+    cout << "3. Multiplication" << endl;
+    cout << "4. Division" << endl;
+
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    switch(choice) {
+        case 1:
+            cout << "Result = " << a + b;
+            break;
+
+        case 2:
+            cout << "Result = " << a - b;
+            break;
+
+        case 3:
+            cout << "Result = " << a * b;
+            break;
+
+        case 4:
+            if(b != 0)
+                cout << "Result = " << a / b;
+            else
+                cout << "Division by zero not possible";
+            break;
+
+        default:
+            cout << "Invalid choice";
+    }
+
+    return 0;
+}
+
+
